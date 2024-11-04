44021c0e18b18c105d7c7484b438c99f45c9af43
deybi444 <davelorenzana26@gmail.com>
2024-11-04 19:17:32 +0800

    Create UserProfile.js

---
+++ UserProfile.js
@@ -0,0 +1,8 @@
+const userProfile = {
+    fullName: "Dave Matthew Lardizabal",
+    age: 21,
+    favoriteNumber: 7,
+    favoriteColors: [ "Black", "White", "Blue"]
+};
+
+console.log(userProfile);
