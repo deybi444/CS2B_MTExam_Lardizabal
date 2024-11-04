d51e846238fa0fbdeda2582f4cbf62139380a568
deybi444 <davelorenzana26@gmail.com>
2024-11-04 19:03:08 +0800

    Update classSubject.js

--- classSubject.js
+++ classSubject.js
@@ -5,5 +5,4 @@ const classroom = prompt("Enter classroom:");
 const classInstructor = prompt("Enter class instructor:");
 const studentName = prompt("Enter student name:");
 
-// Log the information to the console
 console.log(`${studentName} is currently enrolled in ${subjectTitle} with a class schedule of ${classSchedule} at room ${classroom}. The instructor for the subject is ${classInstructor}`);
