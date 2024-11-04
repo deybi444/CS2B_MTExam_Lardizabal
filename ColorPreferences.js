56842d66e2708aa622c27b0c88d4b9e5621a6b08
deybi444 <davelorenzana26@gmail.com>
2024-11-04 19:11:14 +0800

    Create  ColorPreferences.js

---
+++ ColorPreferences.js
@@ -0,0 +1,8 @@
+const favoriteColors = [""];
+
+for (let i = 0; i < 5; i++) {
+    const color = prompt("Enter your favorite color:");
+    favoriteColors.push(color);
+
+    console.log("Your favorite colors so far:", favoriteColors);
+}
